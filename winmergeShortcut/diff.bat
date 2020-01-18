a0bbb797cf1aa6828b5947d534cd48aee5cf8380
hidetaka0 <hide.mugu@gmail.com>
2020-01-18 17:21:21 +0900

    Delete useless Fix comment

--- winmergeShortcut/diff.bat
+++ winmergeShortcut/diff.bat
@@ -1,30 +1,17 @@
-:winmerge path
+rem winmerge path
 set WINMERGE="C:\Program Files\WinMerge\WinMergeU.exe"
 
 
-: Working dirctory
+rem Working dirctory
 set WORKDIR=diff
 : filename
 set FILE_A=%WORKDIR%\a.txt
 set FILE_B=%WORKDIR%\%b.txt
 
-: Genereting file name (if you wanto to generate anathor file each time you start this.) 
-:set NOW_DATE=%date:~0,4%%date:~5,2%%date:~8,2%
-:set NOW_TIME=%time:~0,2%%time:~3,2%%time:~6,2%
-:set FILE_A=%WORKDIR%\%NOW_DATE%_%NOW_TIME%_a.txt
-:set FILE_B=%WORKDIR%\%NOW_DATE%_%NOW_TIME%_b.txt
-:type nul > %FILE_A%
-:type nul > %FILE_B%
-
-
-
-echo %FILE_A%
-echo %FILE_B%
-
-: Make working directory
+rem Make working directory
 mkdir %WORKDIR%
 
-: Open text files with the default app
+rem Open text files with the default app
 start %FILE_A%
 start %FILE_B%
 
